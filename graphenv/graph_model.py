f6574c5302c83caa62db1a5d9cf44a95fccb70e8
ctripp <charles.tripp@nrel.gov>
2022-03-03 16:02:07 -0700

    + cleaning up

--- graphenv/graph_model.py
+++ graphenv/graph_model.py
@@ -1,5 +1,5 @@
 from abc import abstractmethod
-from typing import Dict, List, Tuple
+from typing import Dict, Iterable, List, Mapping, Tuple, Union
 
 import graphenv.space_util as space_util
 
@@ -9,6 +9,13 @@ from ray.rllib.utils.framework import try_import_tf
 
 
 tf1, tf, tfv = try_import_tf()
+
+# Type defining the contents of vertex observations as passed to forward()
+GraphModelObservation = Union[
+    tf.Tensor,
+    Iterable["GraphModelObservation"],
+    Mapping[str, "GraphModelObservation"],
+]
 
 
 class GraphModel(TFModelV2):
@@ -30,19 +37,26 @@ class GraphModel(TFModelV2):
         **kwargs,
     ):
         super().__init__(
-            obs_space, action_space, num_outputs, model_config, name, *args, **kwargs
+            obs_space,
+            action_space,
+            num_outputs,
+            model_config,
+            name,
+            *args,
+            **kwargs,
         )
 
+        # observation space key for the action mask
         self._action_mask_key = action_mask_key
+
+        # observation space key for the vertex observations
         self._vertex_observation_key = vertex_observation_key
 
-        self.action_mask = None
-
-        self.current_vertex_value = None
-        self.action_values = None
-
-        self.current_vertex_weight = None
-        self.action_weights = None
+        self.action_mask = None  # bool tensor of valid next actions
+        self.current_vertex_value = None  # value of current vertex
+        self.action_values = None  # values of each action vertex
+        self.current_vertex_weight = None  # weight of current vertex
+        self.action_weights = None  # action weights of each action vertex
 
     def forward(
         self,
@@ -57,25 +71,33 @@ class GraphModel(TFModelV2):
         vertex_observations = observation[self._vertex_observation_key]
         flattened_observations = \
             space_util.flatten_first_dim(vertex_observations)
-        flat_action_values, flat_action_weights = \
-            self.forward_vertex(flattened_observations)
 
-        # Ray likes to make these bool arrays into floats for some unkown reason
+        # flat_values is structured like this: (vertex values, vertex weights)
+        flat_values = self.forward_vertex(flattened_observations)
+
         action_mask = observation[self._action_mask_key]
+
+        # Ray likes to make bool arrays into floats, so we undo it here
         if action_mask.dtype != tf.dtypes.bool:
             action_mask = tf.equal(action_mask, 1.0)
 
         # mask out invalid actions and get current vertex value
-        self.current_vertex_value, self.action_values = \
-            self._get_current_and_mask_action_values(
-                action_mask, flat_action_values)
-        
-        self.current_vertex_weight, self.action_weights = \
-            self._get_current_and_mask_action_values(
-                action_mask, flat_action_weights)
+        def mask_values(values):
+            """
+            Returns the value for the current vertex (index 0 of values),
+            and the masked values of the action verticies.
+            """
+            values = tf.reshape(values, tf.shape(action_mask))
+            current_value = values[:, 0]
+            masked_action_values = \
+                tf.where(action_mask[:, 1:], values[:, 1:], values.dtype.min)
+            return current_value, masked_action_values
+
+        (self.current_vertex_value, self.action_values), \
+            (self.current_vertex_weight, self.action_weights) =\
+            tuple((mask_values(v) for v in flat_values))
 
         self.total_value = self._forward_total_value()
-
         return self.action_weights, state
 
     def value_function(self):
@@ -83,7 +105,8 @@ class GraphModel(TFModelV2):
 
     @abstractmethod
     def forward_vertex(
-        self, input_dict: Dict[str, tf.Tensor]
+        self,
+        input_dict: GraphModelObservation,
     ) -> Tuple[tf.Tensor, tf.Tensor]:
         """
         Forward function returning a value and weight tensor for the verticies
@@ -103,14 +126,3 @@ class GraphModel(TFModelV2):
         the max over all successor states's values.
         """
         return self.current_vertex_value
-
-    def _get_current_and_mask_action_values(self, action_mask, values):
-        """
-        Returns the value for the current vertex (index 0 of values),
-        and the masked values of the action verticies.
-        """
-        values = tf.reshape(values, tf.shape(action_mask))
-        current_value = values[:, 0]
-        masked_action_values = \
-            tf.where(action_mask[:, 1:], values[:, 1:], values.dtype.min)
-        return current_value, masked_action_values
