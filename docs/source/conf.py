9260ee08c760233ac5ed2ba9c9c75cb7b040f7c1
Struan Clark <struan.clark@nrel.gov>
2022-03-30 13:09:53 -0400

    updating to use read-the-docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
+import sphinx_rtd_theme
+
 
 # -- Project information -----------------------------------------------------
 
@@ -38,6 +40,7 @@ extensions = [
     # 'nbsphinx',  # Integrate Jupyter Notebooks and Sphinx
     # 'IPython.sphinxext.ipython_console_highlighting',
     'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
 ]
 
 napoleon_google_docstring = True
@@ -61,7 +64,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
