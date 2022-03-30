c88c77e49f12497fc735d266828b09bde1872c86
Dave Biagioni <dave.biagioni@nrel.gov>
2022-03-30 11:38:39 -0600

    add README and main for script-level demo

--- graphenv/examples/tsp/tsp_env.py
+++ graphenv/examples/tsp/tsp_env.py
@@ -18,3 +18,52 @@ class TSPEnv(GraphEnv):
             max_num_actions=G.number_of_nodes(),
             **kwargs,
         )
+
+
+if __name__ == "__main__":
+
+    import argparse
+    import random
+    import networkx as nx
+
+    from graphenv.examples.tsp.graph_utils import make_complete_planar_graph
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--N", 
+        type=int, 
+        default=5, 
+        help="Number of nodes")
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=None,
+        help="Random seed for creating graph")
+    args = parser.parse_args()
+
+    N = args.N
+    G = make_complete_planar_graph(N=N, seed=args.seed)
+
+    # Solve with networkx heurisitics
+    tsp = nx.approximation.traveling_salesman_problem
+    path = tsp(G, cycle=True)
+    cost = sum([G[path[i]][path[i+1]]["weight"] for i in range(0, N-1)])
+    print(f"Networkx heuristic cost: {cost:1.3f}")
+
+    # Use the TSP env and generate a random circuit
+    config = {"G": G}
+    env = TSPEnv(config)
+
+    def sampler(mask):
+        choices = list(range(len([i for i in mask if i])))
+        return random.choice(choices)
+
+    obs = env.reset()
+    done = False
+    reward = 0.
+    while not done:
+        action = sampler(obs["action_mask"])
+        obs, rew, done, _ = env.step(action)
+        cost -= rew
+    
+    print(f"TSPEnv random action cost: {cost:1.3f}")
