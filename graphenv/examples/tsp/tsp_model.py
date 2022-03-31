97e060cfb3e56f85bffc9b6f91e1be93023c66d1
Dave Biagioni <dave.biagioni@nrel.gov>
2022-03-31 12:31:15 -0600

    fix small typos

--- graphenv/examples/tsp/tsp_model.py
+++ graphenv/examples/tsp/tsp_model.py
@@ -21,7 +21,7 @@ class TSPModel(GraphModel):
         node_idx = layers.Input(shape=(1,), name="node_idx", dtype=tf.int32)
 
         embed_layer = layers.Embedding(
-            num_nodes, hidden_dim, name="embed_layer", input_length=0)
+            num_nodes, hidden_dim, name="embed_layer", input_length=1)
         hidden_layer_1 = layers.Dense(
             hidden_dim, name="hidden_layer_1", activation="relu")
         hidden_layer_2 = layers.Dense(
