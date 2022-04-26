e696265e7510bc05258405a3adb0ef9376c5f932
Dave Biagioni <dave.biagioni@nrel.gov>
2022-04-26 13:25:50 -0600

    why is n=10 failing for me?

--- docs/examples/run-tsp.sh
+++ docs/examples/run-tsp.sh
@@ -4,13 +4,16 @@
 #SBATCH --qos=high
 #SBATCH --time=2:00:00
 #SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=36
 
 clear
 source env.sh
 python run_tsp.py \
     --N=10 \
+    --use-nfp \
     --seed=0 \
     --num-workers 35 \
-    --stop-iters 10000 \
-    --stop-timesteps 1000000
- 
+    --num-gpus 1 \
+    --stop-iters 100000 \
+    --stop-timesteps 10000000
--- docs/examples/run_tsp.py
+++ docs/examples/run_tsp.py
@@ -2,10 +2,13 @@ import argparse
 
 import ray
 from graphenv.examples.tsp.graph_utils import make_complete_planar_graph
-from graphenv.examples.tsp.tsp_env import TSPEnv
+from graphenv.graph_env import GraphEnv
 from graphenv.examples.tsp.tsp_model import TSPModel
+from graphenv.examples.tsp.tsp_state import TSPState
+from graphenv.examples.tsp.tsp_nfp_model import TSPGNNModel
+from graphenv.examples.tsp.tsp_nfp_state import TSPNFPState
 from ray import tune
-from ray.rllib.agents import ppo
+from ray.rllib.agents import ppo, dqn
 from ray.rllib.models import ModelCatalog
 from ray.rllib.utils.framework import try_import_tf
 from ray.rllib.utils.test_utils import check_learning_achieved
@@ -16,20 +19,23 @@ tf1, tf, tfv = try_import_tf()
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "--run", type=str, default="PPO", help="The RLlib-registered algorithm to use."
+    "--run", type=str, default="PPO", choices=["PPO"], 
+    help="The RLlib-registered algorithm to use."
 )
 parser.add_argument("--N", type=int, default=5, help="Number of nodes in TSP network")
 parser.add_argument(
     "--seed", type=int, default=0, help="Random seed used to generate networkx graph"
 )
 parser.add_argument(
-    "--as-test",
+    "--use-nfp",
     action="store_true",
-    help="Whether this script should be run as a test: --stop-reward must "
-    "be achieved within --stop-timesteps AND --stop-iters.",
+    help="Whether to use the GNN model and state.",
 )
 parser.add_argument(
     "--num-workers", type=int, default=1, help="Number of rllib workers"
+)
+parser.add_argument(
+    "--num-gpus", type=int, default=0, help="Number of GPUs"
 )
 parser.add_argument(
     "--stop-iters", type=int, default=50, help="Number of iterations to train."
@@ -59,9 +65,22 @@ if __name__ == "__main__":
 
     ray.init(local_mode=args.local_mode)
 
-    # Can also register the env creator function explicitly with:
-    register_env("tsp", lambda config: TSPEnv(config))
-    ModelCatalog.register_custom_model("my_model", TSPModel)
+    # Customize based on whether using NFP or not.
+    if args.use_nfp:
+        Model = TSPGNNModel
+        State = TSPNFPState
+        custom_model_config = {
+            "num_messages": 1,
+            "embed_dim": 32
+        }
+    else:
+        Model = TSPModel
+        State = TSPState
+        custom_model_config = {
+            "num_nodes": args.N,
+            "hidden_dim": 256,
+            "embed_dim": 256,
+        }
 
     # Create a baseline solution using networkx heuristics
     import networkx as nx
@@ -69,29 +88,40 @@ if __name__ == "__main__":
     N = args.N
     G = make_complete_planar_graph(N=N, seed=args.seed)
 
+    # Compute the reward baseline with heuristic
     tsp = nx.approximation.traveling_salesman_problem
     path = tsp(G, cycle=True)
     reward_baseline = -sum([G[path[i]][path[i + 1]]["weight"] for i in range(0, N - 1)])
     print(f"Networkx heuristic reward: {reward_baseline:1.3f}")
 
-    config = {
-        "env": TSPEnv,  # or "corridor" if registered above
-        "env_config": {"G": G},
+    # Model and env registration.
+    ModelCatalog.register_custom_model("CustomModel", Model)
+
+    # Put the size of the graph in the name
+    env_name = f"tsp-{N}"
+    register_env(env_name, lambda config: GraphEnv(config))
+
+    config = ppo.DEFAULT_CONFIG.copy()
+    
+    config.update({
+        "env": env_name,  # or "corridor" if registered above
+        "env_config": {
+            "state": State(G),
+            "max_num_children": N
+        },
         # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
-        "num_gpus": 0,  # int(os.environ.get("RLLIB_NUM_GPUS", "0")),
+        "num_gpus": args.num_gpus,  # int(os.environ.get("RLLIB_NUM_GPUS", "0")),
         "model": {
-            "custom_model": "my_model",
-            "custom_model_config": {
-                "hidden_dim": 256,
-                "embed_dim": 256,
-                "num_nodes": N,
-            },
+            "custom_model": "CustomModel",
+            "custom_model_config": custom_model_config
         },
         "num_workers": args.num_workers,  # parallelism
         "framework": "tf2",
         "rollout_fragment_length": N,  # a multiple of tour length
-        "train_batch_size": 4 * N * args.num_workers,  # a multiple of num workers
-    }
+        "train_batch_size": 10 * N * args.num_workers,  # a multiple of num workers
+        "entropy_coeff": 0.,
+        "lr": 1e-3
+    })
 
     stop = {
         "training_iteration": args.stop_iters,
@@ -99,35 +129,11 @@ if __name__ == "__main__":
         "episode_reward_mean": min(reward_baseline, args.stop_reward),
     }
 
-    if args.no_tune:
-        # manual training with train loop using PPO and fixed learning rate
-        if args.run != "PPO":
-            raise ValueError("Only support --run PPO with --no-tune.")
-        print("Running manual train loop without Ray Tune.")
-        ppo_config = ppo.DEFAULT_CONFIG.copy()
-        ppo_config.update(config)
-        # use fixed learning rate instead of grid search (needs tune)
-        ppo_config["lr"] = 5e-4
-        trainer = ppo.PPOTrainer(config=ppo_config, env=TSPEnv)
-        # run manual training loop and print results after each iteration
-        for _ in range(args.stop_iters):
-            result = trainer.train()
-            print(pretty_print(result))
-            # stop training of the target train steps or reward are reached
-            if (
-                result["timesteps_total"] >= args.stop_timesteps
-                or result["episode_reward_mean"] >= args.stop_reward
-            ):
-                break
-    else:
-        # automated run with Tune and grid search and TensorBoard
-        print("Training automatically with Ray Tune")
-        results = tune.run(args.run, config=config, stop=stop)
-
-        if args.as_test:
-            print("Checking if learning goals were achieved")
-            check_learning_achieved(results, args.stop_reward)
-
-        results.results_df.to_csv("tsp_results.csv")
+    results = tune.run(
+        args.run,
+        config=config,
+        stop=stop,
+        local_dir="/scratch/dbiagion/ray_results"
+    )
 
     ray.shutdown()
