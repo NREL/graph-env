a6d5b7b0f06b3ebbd2aadb9f412aff9d1fd4b07a
Dave Biagioni <dave.biagioni@nrel.gov>
2022-04-27 10:43:06 -0600

    updating run scripts for eagle

--- docs/examples/run_tsp.py
+++ docs/examples/run_tsp.py
@@ -41,6 +41,12 @@ parser.add_argument(
     "--num-gpus", type=int, default=0, help="Number of GPUs"
 )
 parser.add_argument(
+    "--lr", type=float, default=1e-4, help="learning rate"
+)
+parser.add_argument(
+    "--entropy-coeff", type=float, default=0., help="entropy coefficient"
+)
+parser.add_argument(
     "--stop-iters", type=int, default=50, help="Number of iterations to train."
 )
 parser.add_argument(
@@ -66,48 +72,20 @@ if __name__ == "__main__":
     N = args.N
     G = make_complete_planar_graph(N=N, seed=args.seed)
 
-    # # Compute the reward baseline with heuristic
-    # import networkx as nx
-    # tsp_approx = nx.approximation.traveling_salesman_problem
-    # path = tsp_approx(G, cycle=True)
-    # reward_baseline = -sum([G[path[i]][path[i + 1]]["weight"] for i in range(0, N - 1)])
-    # print(f"Networkx heuristic reward: {reward_baseline:1.3f}")
-
-    # # Register model and env
-    # ModelCatalog.register_custom_model("TSPGNNModel", TSPGNNModel)
-
-    # env_name = f"tsp-{N}"
-    # register_env(env_name, lambda config: GraphEnv(config))
-
-    # config = {
-    #     "env": env_name,
-    #     "env_config": {
-    #         "state": TSPNFPState(G),
-    #         "max_num_children": N
-    #     },
-    #     "model": {
-    #         "custom_model": "TSPGNNModel",
-    #         "custom_model_config": {
-    #             "num_messages": 1,
-    #             "embed_dim": 32
-    #         }
-    #     },
-    #     "num_workers": args.num_workers,  # parallelism
-    #     "num_gpus": args.num_gpus,
-    #     "framework": "tf2",
-    #     "eager_tracing": False,
-    #     "eager_max_retraces": 20,
-    #     "rollout_fragment_length": N,  # a multiple of tour length
-    #     "train_batch_size": 10 * N * args.num_workers,  # a multiple of num workers
-    #     "entropy_coeff": 0.,
-    #     "lr": 1e-3
-    # }
+    # Compute the reward baseline with heuristic
+    import networkx as nx
+    tsp_approx = nx.approximation.traveling_salesman_problem
+    path = tsp_approx(G, cycle=True)
+    reward_baseline = -sum([G[path[i]][path[i + 1]]["weight"] for i in range(0, N - 1)])
+    print(f"Networkx heuristic reward: {reward_baseline:1.3f}")
 
     ModelCatalog.register_custom_model("TSPGNNModel", TSPGNNModel)
-    register_env("graphenv", lambda config: GraphEnv(config))
+
+    env_name = f"graphenv_{N}_lr={args.lr:1.1e}_ec={args.entropy_coeff:1.1e}"
+    register_env(env_name, lambda config: GraphEnv(config))
 
     config = {
-        "env": "graphenv",
+        "env": env_name,
         "env_config": {
             "state": TSPNFPState(G),
             "max_num_children": G.number_of_nodes(),
@@ -119,35 +97,33 @@ if __name__ == "__main__":
                 "embed_dim": 32,
             },
         },
+        "num_workers": args.num_workers,  # parallelism
+        "num_gpus": args.num_gpus,
+        "framework": "tf2",
+        "eager_tracing": False,
+        "rollout_fragment_length": N,  # a multiple of N (collect whole episodes)
+        "train_batch_size": 10 * N * args.num_workers,  # a multiple of N * num workers
+        "entropy_coeff": args.entropy_coeff,
+        "sgd_minibatch_size": 128,
+        "num_sgd_iter": 5,
+        "lr": args.lr,
         "log_level": "DEBUG"
     }
 
     ppo_config = ppo.DEFAULT_CONFIG.copy()
     ppo_config.update(config)
-    trainer = ppo.PPOTrainer(config=ppo_config)
-    result = trainer.train()
 
-    # stop = {
-    #     "training_iteration": args.stop_iters,
-    #     "timesteps_total": args.stop_timesteps,
-    #     "episode_reward_mean": args.stop_reward,
-    # }
+    stop = {
+        "training_iteration": args.stop_iters,
+        "timesteps_total": args.stop_timesteps,
+        "episode_reward_mean": args.stop_reward,
+    }
 
-    # results = tune.run(
-    #     args.run,
-    #     config=ppo_config,
-    #     stop=stop,
-    #     local_dir="/scratch/dbiagion/ray_results"
-    # )
-
-    # for _ in range(stop["training_iteration"]):
-    #     result = trainer.train()
-    #     print(pretty_print(result))
-    #     # stop training of the target train steps or reward are reached
-    #     if (
-    #         result["timesteps_total"] >= stop["stop_timesteps"]
-    #         or result["episode_reward_mean"] >= stop["episode_reward_mean"]
-    #     ):
-    #         break
+    tune.run(
+        args.run,
+        config=ppo_config,
+        stop=stop,
+        local_dir="/scratch/dbiagion/ray_results"
+    )
 
     ray.shutdown()
