94fae4d9b6af83debf0702611c0cc21dd583c45d
Peter St. John <peterc.stjohn@gmail.com>
2022-04-28 16:06:46 -0600

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import ray
 from ray.rllib.agents import dqn, ppo
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 # @pytest.fixture
 def ray_init():
     ray.init(num_cpus=1, local_mode=True)
@@ -12,45 +12,45 @@ def ray_init():
 
 
 @pytest.fixture
-def ppo_config():
-
-    config = {
+def global_config():
+    return {
         "num_gpus": 0,
-        "num_workers": 1,  # parallelism
+        "num_workers": 1,
         "framework": "tf2",
         "eager_tracing": False,
         "eager_max_retraces": 20,
         "rollout_fragment_length": 5,
         "train_batch_size": 20,
-        "sgd_minibatch_size": 2,
-        "shuffle_sequences": True,
-        "num_sgd_iter": 1,
         "lr": 1e-3,
     }
 
-    ppo_config = ppo.DEFAULT_CONFIG.copy()
-    ppo_config.update(config)
 
-    return ppo_config
+@pytest.fixture
+def ppo_config(global_config):
+
+    specific_config = {
+        "sgd_minibatch_size": 2,
+        "shuffle_sequences": True,
+        "num_sgd_iter": 1,
+    }
+
+    config = ppo.DEFAULT_CONFIG.copy()
+    config.update(global_config)
+    config.update(specific_config)
+
+    return config
 
 
 @pytest.fixture
-def dqn_config():
+def dqn_config(global_config):
 
-    config = {
-        "num_gpus": 0,
-        "num_workers": 1,  # parallelism
-        "framework": "tf2",
-        "eager_tracing": False,
-        "eager_max_retraces": 20,
-        "rollout_fragment_length": 5,
-        "train_batch_size": 20,
-        "lr": 1e-3,
+    specific_config = {
         "timesteps_per_iteration": 100,
         "target_network_update_freq": 50,
     }
 
-    dqn_config = dqn.DEFAULT_CONFIG.copy()
-    dqn_config.update(config)
+    config = dqn.DEFAULT_CONFIG.copy()
+    config.update(global_config)
+    config.update(specific_config)
 
-    return dqn_config
+    return config
