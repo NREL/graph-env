eb9dd75d3fa04adaf18395baa0feedc630118e77
Peter St. John <peterc.stjohn@gmail.com>
2022-07-12 17:05:25 -0600

    Update __init__.py

--- graphenv/__init__.py
+++ graphenv/__init__.py
@@ -12,3 +12,9 @@ tf1: Optional[ModuleType] = None
 
 tf1, tf, tfv = try_import_tf()
 torch, nn = try_import_torch()
+
+if tf1 is not None:
+    assert tfv == 2
+    assert tf is not None
+    if not tf1.executing_eagerly():
+        tf.compat.v1.enable_eager_execution()
