098c1ae910db8e907835297723d7a673a320258c
pstjohn <peterc.stjohn@gmail.com>
2022-07-13 15:40:45 +0000

    Deploying to gh-pages from @ NREL/graph-env@4b9847015d99bc4bd61f2ca5963a805c5f00c4ef 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g612b1d9',
+    VERSION: '0+untagged.1.g4b98470',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
