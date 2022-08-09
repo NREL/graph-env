d468793aca2d66c788f605d85ebd3f5aa170e80e
Peter St. John <peterc.stjohn@gmail.com>
2022-08-09 11:15:59 -0600

    Update __init__.py

--- graphenv/__init__.py
+++ graphenv/__init__.py
@@ -1,6 +1,10 @@
 from ray.rllib.utils.framework import try_import_tf, try_import_torch
 
-from graphenv._version import __version__, __version_tuple__  # noqa: F401
+try:
+    from graphenv._version import __version__, __version_tuple__  # noqa: F401
+except ModuleNotFoundError:
+    __version__ = "package not installed"
+    __version_tuple__ = (None, None, None)
 
 tf1, tf, tfv = try_import_tf()
 assert tfv == 2
