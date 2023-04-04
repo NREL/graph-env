af64c046ba5dd6e56a10f7b63c05f5ea65aa55f2
jlaw9 <jeffreynlaw@gmail.com>
2023-04-04 17:51:34 +0000

    Deploying to gh-pages from @ NREL/graph-env@6baed4057fe1599a27fe9f28b0a477e89afc446e 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.4.dev1+gf403690',
+    VERSION: '0.2.4.dev2+g6baed40',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
